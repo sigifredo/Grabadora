bf93512fd0a0b161d988d2e12884bde0537bc7de
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-04-20 01:46:52 -0500

    RecordsFragment added.

--- app/src/main/java/com/eldamar/grabadora/app/SaveFileActivity.java
+++ app/src/main/java/com/eldamar/grabadora/app/SaveFileActivity.java
@@ -28,7 +28,6 @@ public class SaveFileActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.save_file, menu);
         return true;
