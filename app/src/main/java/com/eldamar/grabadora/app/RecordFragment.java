989af73f01e378e99aa14a2c782bb832050359b6
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-05-11 14:40:48 -0500

    The time show of toast was changed.

--- app/src/main/java/com/eldamar/grabadora/app/RecordFragment.java
+++ app/src/main/java/com/eldamar/grabadora/app/RecordFragment.java
@@ -99,9 +99,9 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
                                 File oFile = new File(getActivity().getFilesDir(), fileName);
 
                                 if (mFile.renameTo(oFile))
-                                    Toast.makeText(getActivity(), "Se ha guardaro el archivo satisfactoriamente.", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(getActivity(), "Se ha guardaro el archivo satisfactoriamente.", Toast.LENGTH_SHORT).show();
                                 else
-                                    Toast.makeText(getActivity(), "Ha ocurrido un problema, y el archivo no pudo ser guardado.", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(getActivity(), "Ha ocurrido un problema, y el archivo no pudo ser guardado.", Toast.LENGTH_SHORT).show();
                             }
                         }
                     })
