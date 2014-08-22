ded84ff95f7209860e275f32a32af37291d92e18
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-08-22 02:52:54 -0500

    SaveRecordDialog now does not save file, only get the file name.
    Before it change, this class saved the file. Now, this class only get the file name.

--- app/src/main/java/com/nullpoint/recorder/gui/SaveRecordDialog.java
+++ app/src/main/java/com/nullpoint/recorder/gui/SaveRecordDialog.java
@@ -5,15 +5,26 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.widget.EditText;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class SaveRecordDialog extends AlertDialog {
 
+    public static final int ACCEPT = 1;
+    public static final int CANCEL = 1;
+
+    private int mButtonPressed;
     private EditText mEditText;
+    private String mFileName;
 
     public SaveRecordDialog(Context context) {
         super(context);
 
+        mButtonPressed = CANCEL;
         mEditText = new EditText(context);
-        SaveListener listener = new SaveListener();
+        mFileName = "";
+        SaveListener listener = new SaveListener(this);
+
         setView(mEditText);
         setTitle("Guardar archivo");
         setMessage("Nombre del archivo");
@@ -22,34 +33,38 @@ public class SaveRecordDialog extends AlertDialog {
         setButton(BUTTON_NEGATIVE, context.getText(android.R.string.cancel), listener);
     }
 
+    public int getButtonPressed() {
+        return mButtonPressed;
+    }
+
+    public String getFileName() {
+        return mFileName;
+    }
+
     class SaveListener implements DialogInterface.OnClickListener {
 
-        // private RecordFragment mRecordFragment;
+        private SaveRecordDialog mDialog;
 
-        public SaveListener(/*RecordFragment recordFragment*/) {
-            // mRecordFragment = recordFragment;
-        }
-
-        protected void cancel() {
-            // mRecordFragment.mRecorder.clearCache();
+        public SaveListener(SaveRecordDialog dialog) {
+            mDialog = dialog;
         }
 
         @Override
         public void onClick(DialogInterface dialog, int which) {
             if (which == DialogInterface.BUTTON_POSITIVE)
-                save(dialog);
+                save();
             else
-                cancel();
+                mDialog.mButtonPressed = CANCEL;
         }
 
-        protected void save(DialogInterface dialog) {
+        protected void save() {
+            mDialog.mFileName = mDialog.mEditText.getText().toString();
+
+            if (mDialog.mFileName.isEmpty())
+                mDialog.mFileName = DateFormat.getDateTimeInstance().format(new Date());
+
+            mDialog.mButtonPressed = ACCEPT;
             /*
-            AlertDialog alertDialog = (AlertDialog) dialog;
-            String fileName = et.getText().toString().trim();
-
-            if (fileName.isEmpty())
-                fileName = DateFormat.getDateTimeInstance().format(new Date());
-
             try {
                 if (mRecordFragment.mRecorder.saveRecord(fileName))
                     Toast.makeText(getActivity(), "Se ha guardaro el archivo satisfactoriamente.", Toast.LENGTH_SHORT).show();
