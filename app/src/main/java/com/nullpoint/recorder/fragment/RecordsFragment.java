f5f8554dbb9014d9efeb17f63de69f7746b03757
System User <obed.gonzalez@lenovo.(none)>
2014-08-23 17:38:57 -0500

    The MultiChoiceModeListener class was created.
    This class replaces the code into RecordsFragment that handles the actions in the list of records.

--- app/src/main/java/com/nullpoint/recorder/fragment/RecordsFragment.java
+++ app/src/main/java/com/nullpoint/recorder/fragment/RecordsFragment.java
@@ -54,88 +54,7 @@ public class RecordsFragment extends ListFragment {
         super.onActivityCreated(savedInstanceState);
 
         getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-        getListView().setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
-
-            HashSet<Integer> mItems = new HashSet<Integer>();
-
-            @Override
-            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
-                int n = getListView().getCheckedItemCount();
-                actionMode.setTitle(R.string.select_items);
-                if (n == 1)
-                    actionMode.setSubtitle("1 " + getString(R.string.file_selected));
-                else
-                    actionMode.setSubtitle("" + n + " " + getString(R.string.files_selected));
-
-                if (b)
-                    mItems.add(i);
-                else
-                    mItems.remove(i);
-            }
-
-            @Override
-            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
-                MenuInflater menuInflater = actionMode.getMenuInflater();
-                menuInflater.inflate(R.menu.multiple_select_bar, menu);
-                return true;
-            }
-
-            @Override
-            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
-                return false;
-            }
-
-            @Override
-            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.action_delete:
-                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                        builder.setMessage("¿Está seguro de querer eliminar las grabaciones?")
-                                .setTitle("Eliminar archivos")
-                                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int iButton) {
-                                        int i = 0;
-                                        Iterator<Integer> it = mItems.iterator();
-                                        String [] paths = new String[mItems.size()];
-                                        final String activityPath = getActivity().getFilesDir().getAbsolutePath() + "/";
-
-                                        while (it.hasNext())
-                                            paths[i++] = activityPath + getListAdapter().getItem(it.next());
-
-                                        mListener.deleteRecords(paths);
-                                        loadFiles();
-                                    }
-                                })
-                                .setNegativeButton(android.R.string.cancel, null)
-                                .create().show();
-                        actionMode.finish();
-                        return true;
-                    case R.id.action_share:
-                        Intent shareIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
-                        ArrayList<Uri> uris = new ArrayList<Uri>();
-                        final String recordsPath = getActivity().getFilesDir().getAbsolutePath() + "/";
-                        Iterator<Integer> it = mItems.iterator();
-
-                        while (it.hasNext()) {
-                            String name = (String) getListAdapter().getItem(it.next());
-                            uris.add(Uri.fromFile(new File(recordsPath + name)));
-                        }
-
-                        shareIntent.setType("audio/*");
-                        shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.app_name));
-                        shareIntent.putExtra(Intent.EXTRA_STREAM, uris);
-                        startActivity(Intent.createChooser(shareIntent, "Share Nullpoint"));
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-
-            @Override
-            public void onDestroyActionMode(ActionMode actionMode) {
-            }
-        });
+        getListView().setMultiChoiceModeListener(new MultiChoiceModeListener());
     }
 
     @Override
@@ -145,7 +64,7 @@ public class RecordsFragment extends ListFragment {
             mListener = (InteractionListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
-                + " must implement InteractionListener");
+                                         + " must implement InteractionListener");
         }
     }
 
