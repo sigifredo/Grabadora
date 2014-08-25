a2d9113ab8f30284cc5d5b594f07b1ce542c4da7
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-08-25 17:33:02 -0500

    We are working to fix the errors of the MultiChoiceModeListener.

--- app/src/main/java/com/nullpoint/recorder/util/MultiChoiceModeListener.java
+++ app/src/main/java/com/nullpoint/recorder/util/MultiChoiceModeListener.java
@@ -1,43 +1,50 @@
 package com.nullpoint.recorder.util;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.AbsListView;
+
+import com.nullpoint.recorder.fragment.RecordsFragment;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class MultiChoiceModeListener implements AbsListView.MultiChoiceModeListener {
 
+    RecordsFragment mRecordsFragment;
     HashSet<Integer> mItems = new HashSet<Integer>();
 
-    public boolean deleteRecords() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+    public MultiChoiceModeListener(RecordsFragment recordsFragment) {
+        mRecordsFragment = recordsFragment;
+    }
+
+    public void deleteRecords() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mRecordsFragment.getActivity());
+        DialogListener listener = new DialogListener(this);
+
         builder.setMessage("¿Está seguro de querer eliminar las grabaciones?")
         .setTitle("Eliminar archivos")
-        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int iButton) {
-                int i = 0;
-                Iterator<Integer> it = mItems.iterator();
-                String [] paths = new String[mItems.size()];
-                final String activityPath = getActivity().getFilesDir().getAbsolutePath() + "/";
-
-                while (it.hasNext())
-                    paths[i++] = activityPath + getListAdapter().getItem(it.next());
-
-                mListener.deleteRecords(paths);
-                loadFiles();
-            }
-        })
+        .setPositiveButton(android.R.string.ok, listener)
         .setNegativeButton(android.R.string.cancel, null)
         .create().show();
-        actionMode.finish();
-
-        return true;
     }
 
     public boolean shareRecords() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
         ArrayList<Uri> uris = new ArrayList<Uri>();
-        final String recordsPath = getActivity().getFilesDir().getAbsolutePath() + "/";
+        final String recordsPath = mRecordsFragment.getActivity().getFilesDir().getAbsolutePath() + "/";
         Iterator<Integer> it = mItems.iterator();
 
         while (it.hasNext()) {
-            String name = (String) getListAdapter().getItem(it.next());
+            String name = (String) mRecordsFragment.getListAdapter().getItem(it.next());
             uris.add(Uri.fromFile(new File(recordsPath + name)));
         }
 
@@ -51,7 +58,7 @@ public class MultiChoiceModeListener implements AbsListView.MultiChoiceModeListe
 
     @Override
     public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
-        int n = getListView().getCheckedItemCount();
+        int n = mRecordsFragment.getListView().getCheckedItemCount();
         actionMode.setTitle(R.string.select_items);
 
         if (n == 1)
@@ -81,7 +88,9 @@ public class MultiChoiceModeListener implements AbsListView.MultiChoiceModeListe
     public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
         switch (menuItem.getItemId()) {
         case R.id.action_delete:
-            return deleteRecords();
+            deleteRecords();
+            actionMode.finish();
+            return true;
         case R.id.action_share:
             return shareRecords();
         default:
@@ -91,5 +100,27 @@ public class MultiChoiceModeListener implements AbsListView.MultiChoiceModeListe
 
     @Override
     public void onDestroyActionMode(ActionMode actionMode) {
+    }
+
+    class DialogListener implements DialogInterface.OnClickListener {
+        private MultiChoiceModeListener mMultiChoiceModeListener;
+
+        public DialogListener(MultiChoiceModeListener multiChoiceModeListener) {
+            mMultiChoiceModeListener = multiChoiceModeListener;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialogInterface, int iButton) {
+            int i = 0;
+            Iterator<Integer> it = mItems.iterator();
+            String [] paths = new String[mItems.size()];
+            final String activityPath = mMultiChoiceModeListener.mContext.getFilesDir().getAbsolutePath() + "/";
+
+            while (it.hasNext())
+                paths[i++] = activityPath + mMultiChoiceModeListener.mRecordsFragment.getListAdapter().getItem(it.next());
+
+            mListener.deleteRecords(paths);
+            mMultiChoiceModeListener.mRecordsFragment.loadFiles();
+        }
     }
 }
